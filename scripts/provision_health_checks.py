a47c9dc2a0bee852df5d85f1040506fcd29f14d2
Abraham Chavez <achave11@ucsc.edu>
2019-09-06 16:19:14 -0700

    fixup! Provision Azul health checks in DCP wide composite health check #(1150)

--- scripts/provision_health_checks.py
+++ scripts/provision_health_checks.py
@@ -15,8 +15,8 @@ logger = logging.getLogger(__name__)
 
 
 def main(argv):
-    parser = argparse.ArgumentParser(description='Dynamically reference and dereference Route53 child health checks '
-                                                 'not managed by Azul.')
+    parser = argparse.ArgumentParser(description='Dynamically reference or dereference Azul\'s Route53 health checks '
+                                                 'to/from composite health checkers not managed by Azul.')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--reference', '-R', dest='reference', action='store_true', default=False,
                        help='Link Azul managed Route53 health check resources to DCP wide composite health check.')
@@ -29,12 +29,13 @@ def main(argv):
     except KeyError:
         logger.info(f'DCP wide health check does not exist for {config.deployment_stage}')
     else:
-        logger.info('Referencing HealthChecks')
         provision_health_check(health_checks, dcp_wide_health_check, options.reference)
 
 
 def get_health_checks():
-    client, health_checks = health_check_ops()
+    client = boto3.client('route53')
+    paginator = client.get_paginator('list_health_checks')
+    health_checks = chain.from_iterable(page['HealthChecks'] for page in paginator.paginate())
     health_check_batches = more_itertools.chunked(health_checks, 10)
     dcp_health_checks = {}
     for health_check_batch in health_check_batches:
@@ -51,30 +52,22 @@ def get_health_checks():
     return dcp_health_checks
 
 
-def health_check_ops():
-    client = boto3.client('route53')
-    paginator = client.get_paginator('list_health_checks')
-    return client, chain.from_iterable(page['HealthChecks'] for page in paginator.paginate())
-
-
 def provision_health_check(health_checks, dcp_health_check, reference):
-    dcp_health_check_config = dcp_health_check['HealthCheckConfig']
-    azul_service = health_checks[config.service_name]
-    azul_indexer = health_checks[config.indexer_name]
-    azul_data_browser = health_checks[config.data_browser_name]
-    azul_data_portal = health_checks[config.data_portal_name]
-    updated_child_health_checks = [health_check for health_check in dcp_health_check_config['ChildHealthChecks']
-                                   if health_check not in (azul_service['Id'],
-                                                           azul_indexer['Id'],
-                                                           azul_data_browser['Id'],
-                                                           azul_data_portal['Id'])]
-    if reference:
-        updated_child_health_checks.append(azul_service['Id'])
-        updated_child_health_checks.append(azul_indexer['Id'])
-        updated_child_health_checks.append(azul_data_browser['Id'])
-        updated_child_health_checks.append(azul_data_portal['Id'])
-    response = update_dcp_wide_health_check(dcp_health_check, updated_child_health_checks)
-    logger.info(json.dumps(response))
+    dcp_health_check_children = dcp_health_check['HealthCheckConfig']['ChildHealthChecks']
+    try:
+        azul_health_checks = [health_checks[resource_name]['Id'] for resource_name in (config.service_name,
+                                                                                       config.indexer_name,
+                                                                                       config.data_browser_name,
+                                                                                       config.data_portal_name)]
+    except KeyError:
+        logger.info(f'No Azul health checks found for {config.deployment_stage} deployment!')
+    else:
+        dcp_health_check_children = [health_check for health_check in dcp_health_check_children
+                                     if health_check not in azul_health_checks]
+        if reference:
+            dcp_health_check_children.extend(azul_health_checks)
+        response = update_dcp_wide_health_check(dcp_health_check, dcp_health_check_children)
+        logger.info(json.dumps(response))
 
 
 def update_dcp_wide_health_check(dcp_wide_health_check, updated_child_health_checks):
